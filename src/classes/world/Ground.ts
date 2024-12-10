e869f3ee3139939e47696c128f47d9285d16eda0
Jack Kingham <kinghamjack12@gmail.com>
2024-12-10 21:33:01 +0700

    Simplify Starter Kit

--- src/classes/world/Ground.ts
+++ src/classes/world/Ground.ts
@@ -9,32 +9,20 @@ export default class Ground {
 
     parent: World;
     _mesh!: THREE.Mesh;
-    _material!: FloorMaterial;
-    _depthMaterial!: FloorShadowMaterial;
 
     constructor(parent: World) {
         this.parent = parent;
 
         this._init();
-        this.debug();
-    }
-
-    debug = () => {
-        const folder = this.parent.parent._debug.addFolder("Ground")
-
-        folder.addColor(this._material.uniforms.uValleyColor, "value").name("uValleyColor")
-        folder.addColor(this._material.uniforms.uPeakColor, "value").name("uPeakColor")
     }
 
     _init = () => {
-        this._material = new FloorMaterial();
-        this._depthMaterial = new FloorShadowMaterial();
+        this._mesh = new THREE.Mesh(
+            this._geometry(),
+            new THREE.MeshStandardMaterial({ color: "lightgreen" })
+        );
 
-        this._mesh = new THREE.Mesh(this._geometry(), this._material as unknown as THREE.Material);
-        this._mesh.customDepthMaterial = this._depthMaterial as unknown as THREE.Material;
-
-        this._mesh.receiveShadow = true;
-        this._mesh.geometry.rotateX(-Math.PI / 2);
+        this._mesh.rotateX(-Math.PI / 2);
     }
 
     _geometry = () => {
